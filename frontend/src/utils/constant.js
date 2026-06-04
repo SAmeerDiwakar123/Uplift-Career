a81270f15bfc28f9573be1fcefebaec5d6757ced
Sameer Diwakar <sameerdiwakar2003@gmail.com>
2026-06-04 22:52:49 +0530

    update frontend api url

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,4 @@
-const BASE_URL = import .meta.env.VITE_BACKEND_URL;
+const BASE_URL = import.meta.env.VITE_BACKEND_URL;
 
 export const USER_API_END_POINT = `${BASE_URL}/api/v1/user`;
 export const JOB_API_END_POINT = `${BASE_URL}/api/v1/job`;
