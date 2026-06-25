50d6b5e32cee316da19b589f37d518560e6d9383
Sameer Diwakar <sameerdiwakar2003@gmail.com>
2026-06-25 09:09:54 +0530

    update all

--- server/utils/razorpay.js
+++ server/utils/razorpay.js
@@ -1,9 +1,4 @@
-// ✅ Razorpay ko safely initialize karo
 import Razorpay from "razorpay";
-
-if (!process.env.RAZORPAY_KEY_ID || !process.env.RAZORPAY_KEY_SECRET) {
-  throw new Error("Razorpay keys missing in environment variables");
-}
 
 const razorpay = new Razorpay({
   key_id: process.env.RAZORPAY_KEY_ID,
