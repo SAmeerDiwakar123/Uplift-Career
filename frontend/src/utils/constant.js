910aa7475e24b2cb0dbd0efa6eecdabf24bb1811
Sameer Diwakar <sameerdiwakar2003@gmail.com>
2026-06-28 23:53:05 +0530

    update all

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -7,10 +7,25 @@
 // export const ORDER_API_END_POINT = "http://localhost:5000/api/v1/order";
 
 
-export const USER_API_END_POINT="https://uplift-career-backend.vercel.app/api/v1/user";
-export const JOB_API_END_POINT="https://uplift-career-backend.vercel.app/api/v1/job";
-export const APPLICATION_API_END_POINT="https://uplift-career-backend.vercel.app/api/v1/application";
-export const COMPANY_API_END_POINT="https://uplift-career-backend.vercel.app/api/v1/company";
+// export const USER_API_END_POINT="https://uplift-career-backend.vercel.app/api/v1/user";
+// export const JOB_API_END_POINT="https://uplift-career-backend.vercel.app/api/v1/job";
+// export const APPLICATION_API_END_POINT="https://uplift-career-backend.vercel.app/api/v1/application";
+// export const COMPANY_API_END_POINT="https://uplift-career-backend.vercel.app/api/v1/company";
+// export const COURSE_API_END_POINT = "https://uplift-career-backend.vercel.app/api/v1/course";
+// export const ENROLLMENT_API_END_POINT = "https://uplift-career-backend.vercel.app/api/v1/enrollment"
+// export const ORDER_API_END_POINT = "https://uplift-career-backend.vercel.app/api/v1/order";
+
+
+// utils/apiEndpoints.js
+
+export const USER_API_END_POINT = "https://uplift-career-backend.vercel.app/api/v1/user";
+export const ADMIN_API_END_POINT = "https://uplift-career-backend.vercel.app/api/v1/admin";
+export const COMPANY_API_END_POINT = "https://uplift-career-backend.vercel.app/api/v1/company";
+export const JOB_API_END_POINT = "https://uplift-career-backend.vercel.app/api/v1/job";
+export const APPLICATION_API_END_POINT = "https://uplift-career-backend.vercel.app/api/v1/application";
 export const COURSE_API_END_POINT = "https://uplift-career-backend.vercel.app/api/v1/course";
-export const ENROLLMENT_API_END_POINT = "https://uplift-career-backend.vercel.app/api/v1/enrollment"
+export const INTERNSHIP_API_END_POINT = "https://uplift-career-backend.vercel.app/api/v1/internship";
+export const SAVED_API_END_POINT = "https://uplift-career-backend.vercel.app/api/v1/saved";
+export const NOTIFICATION_API_END_POINT = "https://uplift-career-backend.vercel.app/api/v1/notification";
+export const ENROLLMENT_API_END_POINT = "https://uplift-career-backend.vercel.app/api/v1/enrollment";
 export const ORDER_API_END_POINT = "https://uplift-career-backend.vercel.app/api/v1/order";
