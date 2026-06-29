22d1b697b85cb47638a7489c363ac74466a23d0f
Sameer Diwakar <sameerdiwakar2003@gmail.com>
2026-06-29 17:10:02 +0530

    update all

--- server/server.js
+++ server/server.js
@@ -15,8 +15,9 @@ import courseRoute from "./routes/courseRoute.js"
 import enrollmentRoute from "./routes/enrollmentRoute.js"
 import orderRoute from "./routes/orderRoute.js";
 import adminRoute from "./routes/adminRoute.js";
-// import savedJobRoute from "./routes/savedJobRoute.js";
-// import notificationRoute from "./routes/notificationRoute.js";
+import savedJobRoute from "./routes/savedJobRoute.js";
+import notificationRoute from "./routes/notificationRoute.js";
+import { SavedInternship } from "../models/SavedInternshipModel.js";
 
 dns.setServers(["1.1.1.1", "8.8.8.8"]);
 
@@ -75,8 +76,8 @@ app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 app.use("/api/v1/course", courseRoute);
 app.use("/api/v1/internship", internshipRoute)
-// app.use("/api/v1/saved", savedJobRoute);
-// app.use("/api/v1/notification", notificationRoute);
+app.use("/api/v1/saved", savedJobRoute);
+app.use("/api/v1/notification", notificationRoute);
 app.use("/api/v1/enrollment", enrollmentRoute);
 app.use("/api/v1/order", orderRoute);
 
